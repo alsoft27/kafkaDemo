7233cffc763887a8ead0e4c1149234029b146dfb
alsoft27 <alsoft27@gmail.com>
2018-03-25 22:12:25 +0200

    try fix consumer

--- src/main/java/com/alsoft27/kafkademo/services/Consumer.java
+++ src/main/java/com/alsoft27/kafkademo/services/Consumer.java
@@ -2,28 +2,21 @@ package com.alsoft27.kafkademo.services;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
 
-import com.alsoft27.kafkademo.storage.MessageStorage;
-
+@Service
 public class Consumer {
 
 	private static final Logger log = LoggerFactory.getLogger(Consumer.class);
 
-	@Autowired
-	MessageStorage storage;
+	// @Autowired
+	// MessageStorage storage;
 
 	@KafkaListener(topics = "${kafka.cloudkarafka.topic}")
-	public void receive(@Payload String content) {
+	public void receive(String content) {
 		log.info("received content='{}'", content);
-		storage.put(content);
+		// storage.put(content);
 	}
 
-	@Bean
-	public Consumer consumer() {
-		return new Consumer();
-	}
 }
--- src/main/java/com/alsoft27/kafkademo/services/Producer.java
+++ src/main/java/com/alsoft27/kafkademo/services/Producer.java
@@ -4,7 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+@Service
 public class Producer {
 	private static final Logger log = LoggerFactory.getLogger(Producer.class);
 
